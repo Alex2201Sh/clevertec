e228de28bef44201391e70828771c72ba377be7e
Alex2201Sh <shumilov218@gmail.com>
2023-03-06 15:53:58 +0300

    dev: added Lombok

--- src/main/java/by/shumilov/clevertec/bean/Property.java
+++ src/main/java/by/shumilov/clevertec/bean/Property.java
@@ -1,42 +1,10 @@
 package by.shumilov.clevertec.bean;
 
+import lombok.Data;
+
+@Data
 public class Property {
     private String algorithm;
     private int maxLength;
     private int initCapacity;
-
-    public Property() {
-    }
-
-    public String getAlgorithm() {
-        return algorithm;
-    }
-
-    public void setAlgorithm(String algorithm) {
-        this.algorithm = algorithm;
-    }
-
-    public int getMaxLength() {
-        return maxLength;
-    }
-
-    public void setMaxLength(int maxLength) {
-        this.maxLength = maxLength;
-    }
-
-    public int getInitCapacity() {
-        return initCapacity;
-    }
-
-    public void setInitCapacity(int initCapacity) {
-        this.initCapacity = initCapacity;
-    }
-
-    @Override
-    public String toString() {
-        return "Property{" +
-                "algorithm='" + algorithm + '\'' +
-                ", maxLength=" + maxLength +
-                '}';
-    }
 }
